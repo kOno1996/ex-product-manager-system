d96ec8245f0822bb0414b0d9464f7bb77cfd2cee
kOno1996 <keitaro.ono@rakus-partners.co.jp>
2022-06-09 17:55:45 +0900

    メール機能(途中)

--- src/main/java/com/example/demo/controller/ProductBuyController.java
+++ src/main/java/com/example/demo/controller/ProductBuyController.java
@@ -1,5 +1,15 @@
 package com.example.demo.controller;
 
-public class ProductBuyController {
+import org.springframework.mail.MailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/buy")
+public class ProductBuyController {
+	private final MailSender mailSender;
+	
+	public MainController(MailSender mailSender) {
+		this.mailSender = mailSender;
+	}
 }
