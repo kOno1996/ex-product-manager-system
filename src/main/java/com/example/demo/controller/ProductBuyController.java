406c8933bb1fd147d9abbba83c27314fe5bfc290
ono <keitaro.ono@rakus-partners.co.jp>
2022-06-14 02:04:01 +0900

    メール機能(途中)

--- src/main/java/com/example/demo/controller/ProductBuyController.java
+++ src/main/java/com/example/demo/controller/ProductBuyController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/buy")
 public class ProductBuyController {
 	private final MailSender mailSender;
-	
-	public MainController(MailSender mailSender) {
+
+	public ProductBuyController(MailSender mailSender) {
 		this.mailSender = mailSender;
+	}
+
+	@RequestMapping("/mail")
+	public String mail() {
+		SimpleMailMessage msg = new SimpleMailMessage();
+		
+		return "forward:/product-list";
 	}
 }
